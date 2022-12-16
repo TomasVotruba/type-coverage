c3610a256ea70127aa98b27c6b1f61b10fb2e689
Tomas Votruba <tomas.vot@gmail.com>
2022-12-16 16:16:33 +0100

    misc

--- tests/Rules/ParamTypeCoverageRule/ParamTypeCoverageRuleTest.php
+++ tests/Rules/ParamTypeCoverageRule/ParamTypeCoverageRuleTest.php
@@ -37,18 +37,6 @@ final class ParamTypeCoverageRuleTest extends RuleTestCase
         yield [[__DIR__ . '/Fixture/SkipCallableParam.php'], []];
 
         $errorMessage = sprintf(ParamTypeCoverageRule::ERROR_MESSAGE, 3, 0, 80);
-
-        $errorMessage .= '
-
-public function run($name, $age)
-{
-}
-
-public function again($city)
-{
-}
-';
-
         yield [[__DIR__ . '/Fixture/UnknownParamType.php'], [[$errorMessage, -1]]];
     }
 
