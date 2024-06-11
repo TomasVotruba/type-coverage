bdb272eefdf88efa36dfaa1189c1abc89c666e7c
Pablo Schläpfer <pablo@webcraft.ch>
2024-06-11 14:39:40 +0200

    ignore PHP 8.3 only test-fixtures in ECS

--- ecs.php
+++ ecs.php
@@ -6,4 +6,9 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return ECSConfig::configure()
     ->withPaths([__DIR__ . '/src', __DIR__ . '/tests'])
+    ->withSkip([
+        // these fixtures use PHP 8.3 features, we cannot check them wit lower versions
+        __DIR__ . '/tests/Rules/ConstantTypeCoverageRule/Fixture/SkipKnownConstantType.php',
+        __DIR__ . '/tests/Rules/ConstantTypeCoverageRule/Fixture/UnknownConstantType.php',
+    ])
     ->withPreparedSets(common: true, psr12: true, cleanCode: true, symplify: true);
