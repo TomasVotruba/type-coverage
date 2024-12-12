2ce1475049cedb13222267b22c868984e9caaf18
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-12 18:37:37 +0700

    Bump Rector v2 (#48)
    * Update for PHPStan 2.0
    
    * Bump to PHPStan 2
    
    * tidy up
    
    ---------
    
    Co-authored-by: Carlos Granados <barel.barelon@gmail.com>
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

--- tests/Rules/ReturnTypeCoverageRule/ReturnTypeCoverageRuleTest.php
+++ tests/Rules/ReturnTypeCoverageRule/ReturnTypeCoverageRuleTest.php
@@ -16,7 +16,7 @@ final class ReturnTypeCoverageRuleTest extends RuleTestCase
 {
     /**
      * @param string[] $filePaths
-     * @param mixed[] $expectedErrorsWithLines
+     * @param list<array{0: string, 1: int, 2?: string|null}> $expectedErrorsWithLines
      */
     #[DataProvider('provideData')]
     public function testRule(array $filePaths, array $expectedErrorsWithLines): void
